6c4b558119db850e3977e9de2ae5ee88df14d8ca
Boyan Yankov <boian_iankov@abv.bg>
2019-06-01 08:22:23 +0300

    BptEth class created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
-var apiUrl = "https://ropsten.infura.io/bYMbua7UzyBarTHQzOOL";
-var transactionHash = '0x8ae745faa259299245c54e478b4f1bdf4b025f7b29de102fd0331a3ab0405779';
-
-console.log('Starting Blockchain Payload Tools');
-
 Web3 = require('web3');
 
-var web3 = new Web3();
+class BptEth {
 
-web3.setProvider(new web3.providers.HttpProvider(apiUrl));
+  constructor(apiUrl) {
+    this.apiUrl = apiUrl;
+  
+    this.web3 = new Web3();
+    this.web3.setProvider(new this.web3.providers.HttpProvider(apiUrl));
+  }
+  
+  getTransactionPayload(transactionHash){
+    let transaction = this.web3.eth.getTransaction(transactionHash);
+    
+    let result = {
+      payloadHex: transaction.input,
+      payloadAscii: this.toAscii(transaction.input),
+      transactionDetails: transaction,
+    }
+    
+    return result;
+  }
+  
+  toHex(data){
+    return this.web3.toHex(data);
+  }
+  
+  toAscii(data){
+    return this.web3.toAscii(data);
+  }
+  
+}
 
-var transaction = web3.eth.getTransaction(transactionHash);
-console.log(transaction);
+
+let apiUrl = "https://ropsten.infura.io/bYMbua7UzyBarTHQzOOL";
+
+console.log('Starting Blockchain Payload Tools...');
+const bptEth = new BptEth(apiUrl);
+
+console.log('Getting transaction data...');
+let transactionHash = '0x8ae745faa259299245c54e478b4f1bdf4b025f7b29de102fd0331a3ab0405779';
+let transactionPayload = bptEth.getTransactionPayload(transactionHash);
+
+console.log(transactionPayload.payloadAscii);
